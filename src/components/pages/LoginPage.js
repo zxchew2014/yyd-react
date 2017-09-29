6278eedb47152228daea32236db26c26b7b51c8c
Chew Zhi Xuan <zxchew.2014@sis.smu.edu.sg>
2017-09-30 00:43:04 +0800

    Reducer Setup

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import LoginForm from '../forms/LoginForm';
+import { login } from '../../actions/auth';
+import PropTypes from 'prop-types';
 
 class LoginPage extends React.Component {
-  submit = data => {
-    console.log(data);
-  };
+  submit = data =>
+    this.props.login(data).then(() => this.props.history.push('/'));
 
   //Parent of LoginForm
   render() {
@@ -22,4 +24,11 @@ class LoginPage extends React.Component {
 //const LoginPage = () => (
 //);
 
-export default LoginPage;
+LoginPage.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired
+  }).isRequired,
+  login: PropTypes.func.isRequired
+};
+
+export default connect(null, { login })(LoginPage);
--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import 'semantic-ui-css/semantic.min.css';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import rootReducer from './rootReducer';
-import { composeWithDevTools } from 'redux-devtools-extension';
 
 const store = createStore(
   rootReducer,

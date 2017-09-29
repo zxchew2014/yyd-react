c7cb516c5d3533655949f3eac0ee37c1b1b654f5
Chew Zhi Xuan <zxchew.2014@sis.smu.edu.sg>
2017-09-29 16:35:19 +0800

    Semantic UI Setup

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
-
+import 'semantic-ui-css/semantic.min.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 
 ReactDOM.render(
   <BrowserRouter>
-   <App /> 
+   <App />
   </BrowserRouter>
   , document.getElementById('root'));
 registerServiceWorker();

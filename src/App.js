27296500502cf980dec3a2a67937af27c421ca45
Chew Zhi Xuan <zxchew.2014@sis.smu.edu.sg>
2017-10-01 16:20:11 +0800

    Firebase Setup

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import HomePage from './components/pages/HomePage';
 import LoginPage from './components/pages/LoginPage';
+import FBLoginPage from './components/pages/FBLoginPage';
+import DutyOfficerPage from './components/pages/DutyOfficerPage';
 
 const App = () => (
   <div className="ui container">
     <Route path="/" exact component={HomePage} />
     <Route path="/login" exact component={LoginPage} />
+    <Route path="/fblogin" exact component={FBLoginPage} />
+    <Route path="/addDutyOfficer" exact component={DutyOfficerPage} />
   </div>
 );
 
